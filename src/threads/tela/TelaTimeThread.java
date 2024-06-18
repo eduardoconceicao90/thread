9f17de9bde6cc7d025ed51991a290017695a6a84
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-06-18 07:34:33 -0300

    Utilizando o GridBagConstraints gerenciador de posicionamento e layout

--- src/threads/tela/TelaTimeThread.java
+++ src/threads/tela/TelaTimeThread.java
@@ -5,14 +5,44 @@ import java.awt.*;
 
 public class TelaTimeThread extends JDialog {
 
+    private JPanel jPanel = new JPanel(new GridBagLayout()); /* Painel de componentes */
+
+    private JLabel descricaoHora = new JLabel("Time thread 1");
+    private JTextField mostraTempo = new JTextField();
+
+    private JLabel descricaoHora2 = new JLabel("Time thread 2");
+    private JTextField mostraTempo2 = new JTextField();
+
     public TelaTimeThread() { /* Executa o que tiver dentro no momento da abertura ou execução */
-        setTitle("Minha tela de time com Thread");
+        setTitle("Minha tela de time com thread");
         setSize(new Dimension(240, 240));
         setLocationRelativeTo(null);
         setResizable(false);
 
+        //---------------------------------------
 
+        GridBagConstraints gridBagConstraints = new GridBagConstraints(); /* Controlador de posicionamento de componentes */
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
 
+        descricaoHora.setPreferredSize(new Dimension(200,25));
+        jPanel.add(descricaoHora, gridBagConstraints);
+
+        mostraTempo.setPreferredSize(new Dimension(200,25));
+        gridBagConstraints.gridy++;
+        jPanel.add(mostraTempo, gridBagConstraints);
+
+        //---------------------------------------
+
+        descricaoHora2.setPreferredSize(new Dimension(200,25));
+        gridBagConstraints.gridy++;
+        jPanel.add(descricaoHora2, gridBagConstraints);
+
+        mostraTempo2.setPreferredSize(new Dimension(200,25));
+        gridBagConstraints.gridy++;
+        jPanel.add(mostraTempo2, gridBagConstraints);
+
+        add(jPanel, BorderLayout.WEST);
 
         /* Sempre será o último comando */
         setVisible(true); /* Torna a tela visível para o usuário */
